c2966e7ec7fe06483e09269187e19f22d8972295
ChenJohnny1104 <jojostand999@gmail.com>
2026-05-18 01:16:49 +0800

    修正SSR問題

--- app/supabase.js
+++ app/supabase.js
@@ -1,14 +1,8 @@
+'use client'
+
 import { createClient } from '@supabase/supabase-js'
 
-export function getSupabase() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-  return createClient(supabaseUrl, supabaseKey)
-}
-
-export const supabase = typeof window !== 'undefined'
-  ? createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-    )
-  : null as any
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
